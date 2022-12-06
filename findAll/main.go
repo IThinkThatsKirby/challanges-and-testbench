379d3fe561fa9972e07cf1fb595aa2204bcb8da3
kirby ellingson <hkellingson@gmail.com>
2022-12-06 09:16:21 -0600

    more benches

--- findAll/main.go
+++ findAll/main.go
@@ -22,10 +22,32 @@ func findAll(sumOfDigits, numberOfDigits int) []int {
 func digitize(x int) []int {
 	digit := 0
 	digits := []int{}
+	for i := 0; x >= 1; i++ {
+		digit = x % 10
+		x /= 10
+		digits = append(digits, digit)
+	}
+	return digits
+}
+
+func copyDigitize(x int) []int {
+	digit := 0
+	digits := make([]int, 10)
+	for i := 0; x >= 1; i++ {
+		digit = x % 10
+		x /= 10
+		copy(digits[i:], []int{digit})
+	}
+	return digits
+}
+
+func appendDigitze(x int) []int {
+	digit := 0
+	digits := make([]int, 10)
 	for x >= 1 {
 		digit = x % 10
 		x /= 10
-		digits = append([]int{digit}, digits...)
+		digits = append(digits, digit)
 	}
 	return digits
 }
